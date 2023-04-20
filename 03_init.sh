7c6bed297096d84ebfc55515c775660add100279
杜森 <sen.du@petgleam.com>
2023-04-20 10:37:43 +0800

    [IMP] 禁用系统配置

--- 03_init.sh
+++ 03_init.sh
@@ -18,11 +18,64 @@ function install_base() {
     done
 }
 
+# 禁用 hugepage
+function disable_hugepage() {
+    cat config/vm_info | while read ipaddr name passwd
+    do
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "grubby --update-kernel=ALL --args='transparent_hugepage=never'"; 
+        ssh -n $ipaddr "sed -i '/^#RemoveIPC=no/cRemoveIPC=no' /etc/systemd/logind.conf; systemctl restart systemd-logind.service";
+    done
+}
+
+# 关闭 selinux
+function disable_selinux() {
+    cat config/vm_info | while read ipaddr name passwd
+    do
+        ssh -n $ipaddr "sed -i '/^SELINUX=/cSELINUX=disabled' /etc/selinux/config;"; 
+    done
+}
+
+# 配置ssh
+function config_ssh() {
+    cat config/vm_info | while read ipaddr name passwd
+    do
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "sed -i '/^#UseDNS/cUseDNS no' /etc/ssh/sshd_config;";
+        ssh -n $ipaddr "sed -i '/^GSSAPIAuthentication/cGSSAPIAuthentication no' /etc/ssh/sshd_config;";
+        ssh -n $ipaddr "sed -i '/^GSSAPICleanupCredentials/cGSSAPICleanupCredentials no' /etc/ssh/sshd_config;";
+        ssh -n $ipaddr "sed -i '/^#MaxStartups/cMaxStartups 10000:30:20000' /etc/ssh/sshd_config;";
+    done
+}
+
+# 配置网络策略
+function config_network() {
+    cat config/vm_info | while read ipaddr name passwd
+    do
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "chkconfig iptables off; chkconfig ip6tables off; chkconfig postfix off;";
+        ssh -n $ipaddr "systemctl disable postfix; systemctl disable libvirtd; systemctl disable firewalld;";
+        ssh -n $ipaddr "systemctl stop postfix; systemctl stop libvirtd; systemctl stop firewalld;";
+    done
+}
+
 function main() {
     echo -e "$CSTART>03_init.sh$CEND"
 
     echo -e "$CSTART>>install_base$CEND"
     install_base
+
+    echo -e "$CSTART>>stop_hugepage$CEND"
+    disable_hugepage
+
+    echo -e "$CSTART>>disable_selinux$CEND"
+    disable_selinux
+
+    echo -e "$CSTART>>config_ssh$CEND"
+    config_ssh
+
+    echo -e "$CSTART>>config_network$CEND"
+    config_network || true # 忽略报错
 }
 
 main
--- 04_mysql.sh
+++ 04_mysql.sh
@@ -8,12 +8,37 @@
 set -e 
 source 00_env
 
+DATA="/data/mysql"
+
 # 安装 mysql8
 function install_mysql() {
     echo -e "$CSTART>>>>$(hostname -I)$CEND"
     mkdir -p /tmp/mysql8/rpm
     tar -zxvf $ZABBIX_PARCELS/mysql8.0.33.tar.gz -C /tmp/mysql8/rpm
     yum localinstall -y /tmp/mysql8/rpm/*.rpm || true # 忽略报错
+}
+
+# 配置 mysql8
+function config_mysql() {
+    echo -e "$CSTART>>>>$(hostname -I)$CEND"
+    mkdir -p $DATA
+    chown -R mysql $DATA
+    chgrp -R mysql $DATA
+    chmod 755 $DATA
+    cp /etc/my.cnf /etc/my.cnf.bak
+    cp config/mysql /etc/my.cnf
+}
+
+# 重启 mysql8
+function restart_mysql() {
+    echo -e "$CSTART>>>>$(hostname -I)$CEND"
+    systemctl restart mysqld
+    systemctl enable --now mysqld
+}
+
+# 初始化 mysql 数据
+function init_mysql() {
+
 }
 
 function main() {
