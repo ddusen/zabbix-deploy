e5f238aed2f93c5ce6f9c30a717b12b01244df35
杜森 <sen.du@petgleam.com>
2023-04-20 19:15:56 +0800

    [ADD] init_mysql

--- 05_mysql.sh
+++ 05_mysql.sh
@@ -10,7 +10,15 @@ source 00_env
 
 function start_mysql() {
     echo -e "$CSTART>>>>$(hostname -I)$CEND"
-    docker-compose -f mysql8/docker-compose.yml up -d
+    docker-compose -f docker-compose/mysql.yml up -d
+}
+
+function init_mysql() {
+    echo -e "$CSTART>>>>$(hostname -I)$CEND"
+    docker exec -it mysql8 mysql -uroot -p$MYSQL_ROOT_PASSWD -e "create database zabbix character set utf8 collate utf8_bin" || true
+    docker exec -it mysql8 mysql -uroot -p$MYSQL_ROOT_PASSWD -e "create user 'zabbix'@'%' identified by 'Zabbix'"|| true
+    docker exec -it mysql8 mysql -uroot -p$MYSQL_ROOT_PASSWD -e "grant all privileges on zabbix.* to 'zabbix'@'%' with grant option"|| true
+    docker exec -it mysql8 mysql -uroot -p$MYSQL_ROOT_PASSWD -e "alter user 'zabbix'@'%' identified with mysql_native_password by 'Zabbix'"|| true
 }
 
 function main() {
@@ -18,6 +26,9 @@ function main() {
 
     echo -e "$CSTART>>start_mysql$CEND"
     start_mysql
+    
+    echo -e "$CSTART>>init_mysql$CEND"
+    init_mysql
 }
 
 main
