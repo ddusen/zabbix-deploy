6dcb2e5e8c407944693e21933349a924aba64c40
杜森 <sen.du@petgleam.com>
2023-04-21 14:33:36 +0800

    [FIX] 06_zabbix_agent.sh

--- 06_zabbix_agent.sh
+++ 06_zabbix_agent.sh
@@ -15,8 +15,8 @@ function install_agent() {
         echo -e "$CSTART>>>>$ipaddr$CEND";
         scp rpms/pcre2-10.23-2.el7.x86_64.rpm $ipaddr:/tmp/
         scp rpms/zabbix-agent-6.4.1-release1.el7.x86_64.rpm $ipaddr:/tmp/
-        ssh -n $ipaddr "rmp -Uvh /tmp/pcre2-10.23-2.el7.x86_64.rpm" || true
-        ssh -n $ipaddr "rmp -Uvh /tmp/zabbix-agent-6.4.1-release1.el7.x86_64.rpm" || true
+        ssh -n $ipaddr "rpm -Uvh /tmp/pcre2-10.23-2.el7.x86_64.rpm" || true
+        ssh -n $ipaddr "rpm -Uvh /tmp/zabbix-agent-6.4.1-release1.el7.x86_64.rpm" || true
     done
 }
 
